50da34181b71d3f1820ac473b5d2e83f11ba8844
Bart Massey <bart.massey@gmail.com>
2022-10-23 12:41:58 -0700

    cleaned up and fancified

--- singlethread.rs
+++ singlethread.rs
@@ -1,23 +1,22 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
-async fn dp(value: Rc<RefCell<u8>>) {
-    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+async fn dp(id: u64, value: Rc<RefCell<u8>>) {
+    tokio::time::sleep(std::time::Duration::from_millis(1000 - id)).await;
     let mut value = value.borrow_mut();
-    println!("{}", *value);
+    println!("{} {}", id, *value);
     *value += 1;
 }
-
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let value = Rc::new(RefCell::new(0u8));
 
     let local = tokio::task::LocalSet::new();
-
-    local.spawn_local(dp(Rc::clone(&value)));
-    local.spawn_local(dp(Rc::clone(&value)));
+    for id in 0..2 {
+        local.spawn_local(dp(id, Rc::clone(&value)));
+    }
     local.await;
 
-    println!("{}", *value.borrow());
+    println!("{}", Rc::try_unwrap(value).unwrap().into_inner());
 }
